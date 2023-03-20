0c13aec775e9cf4e2adc2eae750cbeb9f6762014
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 09:49:20 -0600

    182 - Formulario Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Login = () => {
-    return "Login";
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('submit');
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Ingrese email" 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Ingrese contraseña" 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
 };
 
 export default Login;
