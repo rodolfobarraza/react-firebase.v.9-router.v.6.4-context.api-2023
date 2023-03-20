95115bb14c655d4540e108a092862cc21d501756
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 10:30:38 -0600

    185 - Si, usuario existe

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-import { useContext } from "react";
-import { createContext } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const UserContext = createContext();
 
 export default function UserContextProvider({ children }) {
     const [user, setUser] = useState(false);
+
+    useEffect(() => {
+        const unsuscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+        })
+        return unsuscribe
+    }, [])
+
+    if (user === false) return <p>Cargando...</p>;
 
     return (
         <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
