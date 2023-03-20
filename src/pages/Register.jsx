80dcd1fc59ac4ae6640b1ef7875ed46117dda669
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 14:52:24 -0600

    199 - Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,10 @@ import { useUserContext } from "../context/UserContext";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import { Avatar, Box, Button, TextField, Typography } from "@mui/material";
+import { AddAPhoto } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
@@ -29,8 +33,16 @@ const Register = () => {
     })
 
     return (
-        <>
-            <h1>Registro de nuevo usuario</h1>
+        <Box sx={{ mt: 8, maxWidth: "400px", mx: "auto", textAlign: "center" }}>
+            <Avatar sx={{ mx: "auto", bgcolor: "#111" }}>
+                <AddAPhoto />
+            </Avatar>
+            <Typography
+                variant="h5"
+                component="h1"
+            >
+                Registro de nuevo usuario
+            </Typography>
             <Formik 
                 initialValues={{ email: '', password: '' }}
                 onSubmit={onSubmit}
@@ -44,29 +56,56 @@ const Register = () => {
                 touched,
                 handleBlur,
             }) => (
-                <form onSubmit={handleSubmit}>
-                    <input 
+                <Box onSubmit={handleSubmit} component="form" sx={{ mt: 1 }}>
+
+                    <TextField
                         type="text" 
-                        placeholder="Ingrese email" 
+                        placeholder="usuario@servidor.com" 
                         value={values.email} 
                         onChange={handleChange}
                         name='email'
                         onBlur={handleBlur}
+                        id='email'
+                        label="Ingresar email"
+                        fullWidth
+                        sx={{ mb:3 }}
+                        error={errors.email && touched.email}
+                        helperText={errors.email && touched.email && errors.email}
                     />
-                    {errors.email && touched.email && errors.email}
-                    <input 
+                    <TextField
                         type="password" 
                         placeholder="Ingrese contraseña" 
                         value={values.password} 
                         onChange={handleChange}
                         name='password'
                         onBlur={handleBlur}
+                        id='password'
+                        label='Ingresar contraseña'
+                        fullWidth
+                        sx={{ mb:3 }}
+                        error={errors.password && touched.password}
+                        helperText={errors.password && touched.password && errors.password}
                     />
-                    {errors.password && touched.password && errors.password}
-                    <button type="submit" disabled={isSubmitting}>Register</button>
-                </form>
+                    <LoadingButton
+                        type="submit"
+                        disabled={isSubmitting}
+                        loading={isSubmitting}
+                        variant="contained"
+                        fullWidth
+                        sx={{ mb: 3 }}
+                    >
+                        Regístrate
+                    </LoadingButton>
+                    <Button
+                        fullWidth
+                        component={Link}
+                        to="/"
+                    >
+                        ¿Ya tienes cuenta?, Ingresa
+                    </Button>
+                </Box>
             )}</Formik>
-        </>
+        </Box>
     )
 };
 
