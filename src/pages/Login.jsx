8939b52e5bbbc08b2741d262586823a09c559e0b
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 10:48:17 -0600

    186 - Redirigir a Dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { login } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+    const {user} = useUserContext()
+
+    useEffect(() => {
+        if (user) {
+            navigate('/dashboard')
+        }
+    }, [user])
 
     const handleSubmit = async(e) => {
         e.preventDefault();
