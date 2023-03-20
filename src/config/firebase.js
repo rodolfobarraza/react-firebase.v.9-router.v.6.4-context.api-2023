b842192414bc47b44245d2bd2e12c979dd6e7688
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-20 11:07:14 -0600

    188 - Cerrar sesión

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -20,4 +20,8 @@ export const login = ({email, password}) => {
 
 export const register = ({email, password}) => {
     return createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const logout = () => {
+    return signOut(auth);
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
+import { logout } from "../config/firebase";
+
 const Dashboard = () => {
-    return "Dashboard";
+
+    const handleLogout = async() => {
+        try {
+            await logout()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            <h1>Dashboard (ruta protegida)</h1>
+            <button onClick={handleLogout}>Logout</button>
+        </>
+    );
 };
 
 export default Dashboard;
